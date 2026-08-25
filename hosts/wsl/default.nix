0943d987ae8caa5b8ca71d5bef65b9823fe4a24f
John Pappas <john.pappas.ctr@amentum.com>
2026-08-25 16:25:46 -0600

    Fix error

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -8,8 +8,6 @@
     ../../modules/containers.nix
     ../../modules/openssh.nix
     ../../modules/amentum.nix
-     # include NixOS-WSL modules
-    <nixos-wsl/modules>
   ];
 
   wsl.enable = true;
